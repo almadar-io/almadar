1f6bd0046253b4a13c2b2134c53b9d693face413
osamah <oalghnmi@gmail.com>
2026-02-18 14:49:36 +0100

    docusaurs config

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -103,6 +103,31 @@ const config: Config = {
     ],
   ],
 
+  headTags: [
+    // LinkedIn Insight Tag
+    {
+      tagName: "script",
+      attributes: { type: "text/javascript" },
+      innerHTML: `
+        _linkedin_partner_id = "YOUR_LINKEDIN_PARTNER_ID";
+        window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+        window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+        (function(l) {
+          if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+          window.lintrk.q=[]}
+          var s = document.getElementsByTagName("script")[0];
+          var b = document.createElement("script");
+          b.type = "text/javascript";b.async = true;
+          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+          s.parentNode.insertBefore(b, s);})(window.lintrk);
+      `,
+    },
+    {
+      tagName: "noscript",
+      innerHTML: `<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=YOUR_LINKEDIN_PARTNER_ID&fmt=gif" />`,
+    },
+  ],
+
   themeConfig: {
     image: "img/og-image.png",
     colorMode: {
