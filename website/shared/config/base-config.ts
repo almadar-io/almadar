1ffd5b70001a9f5640389cda70fb6864c40a3ff1
osamah <oalghnmi@gmail.com>
2026-03-12 14:55:02 +0100

    fix(config): unify all site logos to use the new PNG variants in base-config.ts

--- website/shared/config/base-config.ts
+++ website/shared/config/base-config.ts
@@ -24,7 +24,7 @@ interface SiteConfig {
 }
 
 const SITE_ICONS: Record<string, { logo: string; favicon: string }> = {
-  main: { logo: "img/almadar-icon-transparent.svg", favicon: "img/favicon.ico" },
+  main: { logo: "img/almadar-icon.png", favicon: "img/favicon.ico" },
   orb: { logo: "img/almadar-icon.png", favicon: "img/favicon.ico" },
   studio: { logo: "img/almadar-icon.png", favicon: "img/favicon.ico" },
   services: { logo: "img/almadar-icon.png", favicon: "img/favicon.ico" },
