5143102859326ee1b8c6f4060fec2c677641a6b0
osamah <oalghnmi@gmail.com>
2026-03-16 21:55:47 +0100

    new orb site

--- website/sites/orb/src/theme/MDXComponents.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MDXComponents from "@theme-original/MDXComponents";
-import OrbitalDiagram from "../components/OrbitalDiagram/index";
-
-export default {
-  ...MDXComponents,
-  OrbitalDiagram,
-};
